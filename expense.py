973d7b20e8dc7081a6b102d953ee4970bc666736
Othmane Essakhi <othmane.essakhi@epita.fr>
2021-11-03 20:11:45 +0100

    filling file

--- expense.py
+++ expense.py
@@ -1,4 +1,5 @@
 from PyInquirer import prompt
+import csv
 
 expense_questions = [
     {
@@ -19,12 +20,24 @@ expense_questions = [
 
 ]
 
-
+def fill_file(infos):
+    f = open('expense_report.csv', 'r')
+    nb_lines = 0
+    while(f.readline()):
+        nb_lines += 1
+    with open('expense_report.csv', 'a+', newline='') as csvfile:
+        fieldnames = ['amount', 'label', 'spender']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        if (nb_lines == 0) :
+            print("pass here")
+            writer.writeheader()
+        writer.writerow(infos)
 
 def new_expense(*args):
     infos = prompt(expense_questions)
     # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
+    fill_file(infos)
+    #adding infos to expense file
+    
     print("Expense Added !")
     return True
-
-
